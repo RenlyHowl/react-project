eaae4fc184dd0b5a8796dfa23a2f26b9a1800588
renly <1183957700@qq.com>
2020-01-15 15:51:51 +0800

    基于react-app-rewired和customize-cra自定义配置文件

---
+++ config-overrides.js
@@ -0,0 +1,12 @@
+/* 
+* @file config-overrides.js
+* @author renlyHowl
+* 基于react-app-rewired和customize-cra的定制化配置文件
+* 创建了这个配置文件之后 同时也要对packag.json文件作出修改
+*/
+
+const {override} = require("customize-cra")
+
+module.exports = override(
+
+)
